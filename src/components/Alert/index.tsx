f69ab05dc8c01e98ca5a7aa6ee1ea116c3fec788
josuerx12 <josueaze12@gmail.com>
2024-03-27 12:30:35 -0300

    fix: Import

--- src/components/Alert/index.tsx
+++ src/components/Alert/index.tsx
@@ -1,3 +1,5 @@
+import { FiAlertTriangle } from "react-icons/fi";
+
 type Props = {
   message: string;
   show: boolean;
@@ -5,16 +7,18 @@ type Props = {
   type: string;
 };
 
-const Alert = ({ message, title, show, hidden, type }: Props) => {
+const Alert = ({ message, show, hidden, type }: Props) => {
+  console.log(hidden, type);
   return (
     <div
       className={`relative top-28 ${
         show && "absolute right-6 opacity-100"
       } left-[-700px] z-[999] bg-green-300 border-l-4 border-l-green-800 w-56 p-3 rounded flex flex-col items-center`}
     >
-      <h5 className="text-lg font-bold text-green-950 w-full text-center">
-        Alerta
+      <h5 className="text-lg font-bold text-green-950 w-full text-center flex items-center gap-2">
+        Alerta <FiAlertTriangle />
       </h5>
+      <p>{message}</p>
     </div>
   );
 };
